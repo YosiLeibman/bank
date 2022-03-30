a249e763a7b86ce60a182d4ba4849eabca9ca14b
Yosi Leibman <yosil.770@gmail.com>
2022-03-30 15:01:56 +0300

    logger mw

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const exp = require('express')
 const session = require('express-session')
 const swaggerJsdoc = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express')
+const { logger } = require('./middlewares/logger')
 const { connectToMongo } = require('./mongo/config')
 
 const app = exp()
 
 connectToMongo()
+
+app.use(session({
+    secret: "BlAhbLaH",
+    resave: false,
+    saveUninitialized: true,
+    name:'nothing-to-see-here'
+}))
+
+app.use(logger)
 
 const openApiSpecs = swaggerJsdoc({
     definition:{
@@ -24,14 +34,6 @@ const openApiSpecs = swaggerJsdoc({
 app.use(exp.json())
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(openApiSpecs))
-
-app.use(session({
-    secret: "BlAhbLaH",
-    resave: false,
-    saveUninitialized: true,
-    name:'nothing-to-see-here'
-}))
-
 app.use('/api/accounts', require('./routes/account'))
 app.use('/api/actions', require('./routes/actions'))
 app.use('/api/data', require('./routes/data'))
